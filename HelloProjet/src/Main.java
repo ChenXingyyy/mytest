870215281d8db8368fc659a17cf0e49f8b65cb93
ChenXingyu <1762287852@qq.com>
2019-07-08 21:14:20 +0800

    wer

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,13 +4,8 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-
-
-
         155445
         System.out.println("HelloWorld!");
-        System.out.println("1");
-        System.out.println("H2lloWorld!");
-        System.out.println("He3loWorld!");
+        System.out
     }
 }
